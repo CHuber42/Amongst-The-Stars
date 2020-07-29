04f38bd1d86810f6eb6d32c292f73a2994bf8696
Chris Huber <CHuber42.Work@Gmail.com>
2020-07-29 16:51:19 -0700

    Probably changed some things since last commit but not sure what. Wrapping up current state to experiment with gh pages hosting

--- components/Planet.js
+++ components/Planet.js
@@ -1,7 +1,7 @@
 import CelestialObject from "./CelestialObject";
 import React from "react";
 import {Model, asset, VrButton, View} from "react-360";
-import {PlanetaryScale, CalculateRelativeAngles} from "./GlobalVarsAndFunctions";
+import {PlanetaryScale} from "./GlobalVarsAndFunctions";
 import ContextRing from "./ContextRing";
 
 class Planet extends React.Component
@@ -94,11 +94,12 @@ class Planet extends React.Component
                             globalData={this.props.globalData} 
                             scale={this.style.transform[1].scale}
                           />;
+                      
     if (this.expanded){
       return (
         <VrButton onClick={() => this.toggleExpanded()}>
-        {displayedPlanet}
-        {contextRing}
+          {displayedPlanet}
+          {contextRing}
         </VrButton>
       )
     }

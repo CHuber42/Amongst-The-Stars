34e4f19d264fdf3084badaf09cd14c0baee72f02
Chris Huber <CHuber42.Work@Gmail.com>
2020-08-02 14:22:54 -0700

    refactor context ring style to prevent offset bug

--- components/ContextRing.js
+++ components/ContextRing.js
@@ -1,12 +1,6 @@
 import React from 'react';
-import {StyleSheet, Text, View, VrButton, Image, asset, Environment} from 'react-360';
-import { ImageBackground } from 'react-native';
+import {StyleSheet, View, Image, asset} from 'react-360';
 import {CalculateRelativeAngles} from './GlobalVarsAndFunctions';
-
-/**
- * Render a description of the currently-selected model.
- * Connected to the global store to receive inputs.
- */
 
 
 class ContextRing extends React.Component
@@ -21,29 +15,27 @@ class ContextRing extends React.Component
   }
 
   render(){
+    
     const rotationAngles = CalculateRelativeAngles(this.props.globalData.cameraLocation, this.props.parentCoordinates);
-    const edge = 1200*this.props.scale;
-    let rotation = this.props.globalData.rotation;
-  const translationArray = [(this.props.parentCoordinates[0] - edge/2), (this.props.parentCoordinates[1] + edge/2), this.props.parentCoordinates[2]];
-  const styles = StyleSheet.create({
-    wrapper: {
-      width: edge,
-      height: edge,
-      transform: [
-        {translate: translationArray},
-        {rotateY: rotationAngles[1]},
-        {rotateX: rotationAngles[0]},
-        {rotateZ: rotation},
-        {scale: 1}
-      ],
-      },
-  });
+    const styles = StyleSheet.create({
+      wrapper: {
+        width: .01,
+        height: .01,
+        transform: [
+          {translate: this.props.parentCoordinates},
+          {rotateY: rotationAngles[1]},
+          {rotateX: rotationAngles[0]},
+          {rotateZ: this.props.globalData.rotation},
+          {scale: this.props.scale*120000}
+          ],
+        },
+    });
 
-  return (
-    <View>
-      <Image source={asset('target2.png')} style={styles.wrapper}/>
-    </View>
-  );
+    return (
+      <View>
+        <Image source={asset('target2.png')} style={styles.wrapper}/>
+      </View>
+    );
   }
 }
 
